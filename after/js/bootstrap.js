606bae62ee8135446780d12064276d164bd3eba0
Wakeel Furqan Ahmed <Wakeelfahmed@gmail.com>
2022-08-22 03:36:01 +0500

    lecture 34

--- after/js/bootstrap.js
+++ after/js/bootstrap.js
@@ -531,7 +531,7 @@ if (typeof jQuery === 'undefined') {
 }(jQuery);
 
 /* ========================================================================
- * Bootstrap: collapse.js v3.3.6
+ * Bootstrap:  pse.js v3.3.6
  * http://getbootstrap.com/javascript/#collapse
  * ========================================================================
  * Copyright 2011-2015 Twitter, Inc.
